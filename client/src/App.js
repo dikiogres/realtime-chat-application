a52406a3d79309d11ca5db70b9bbab439fb68989
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-04 15:48:59 +0700

    initialize App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import  App from './App'
+import{ BrowserRouter as Router, Route } from 'react-router-dom'
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+import Join from './component/Join';
+import Chat from './component/Chat';
+
+const App = ()=> (
+   <Router>
+       <Route path="/" exact component={Join}/>
+       <Route path="/chat" component={Chat}/>
+   </Router>
+);
+
+export default App;
