3088117022adc7e0a26db7b59b615bc36f49bdee
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-04 16:38:45 +0700

    empty join component structure

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -5,7 +5,16 @@ const Join = () =>{
     const [room, setRoom] = useState('');
 
     return(
-        <h1>Join</h1>
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="Name" className="joinInput" type="text" onChange={''}/></div>
+                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={''}/></div>
+                <Link>
+                    <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+            </div>
+        </div>
     );
 };
 
