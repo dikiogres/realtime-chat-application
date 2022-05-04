0cb149c910966cc2ac239b9d0d6621ac9be51571
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-04 17:50:00 +0700

    init chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io  from 'socket.io-client';
 
+const Chat = ({ location }) =>{
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
 
-const Chat = () =>{
+    useEffect(() =>{
+        const { name, room } = queryString.parse(location.search);
+
+        setName(name);
+        setRoom(room);     
+    })
+
     return(
         <h1>Chat</h1>
     );
