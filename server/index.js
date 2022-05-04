eff67a7fb71b5d36c4cc8cf10dbb4bbdace1e9d4
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-04 19:25:46 +0700

    rollback react module version

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ const server = http.createServer(app);
 const io = socketio(server); 
 
 io.on('connection', (socket)=>{
-    console.log('we havev a new  connection');
+    console.log('we have a new  connection');
+
+    socket.on('join', ( name, room )=>{
+        console.log(name, room)
+    })
 
     socket.on('disconnect', ()=>{
         console.log('user have disconnected');
